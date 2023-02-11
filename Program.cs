e499261636dd8e3c39a562993a92dc4e7ee52aea
akashbokde98 <akashbokde98@gmail.com>
2023-02-11 16:23:05 +0530

    Added: Initial Push

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestMaximum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Find Maximum Using Generics");
+        }
+    }
+}
