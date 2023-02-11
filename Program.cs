584bc5124faa83913ea07cd7301bd9d339af5c40
akashbokde98 <akashbokde98@gmail.com>
2023-02-11 18:08:23 +0530

    Added: Maximum String Value

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ namespace TestMaximum
         {
             CompareValues compare = new CompareValues();
 
-            string maxValue = compare.MaxStringValue("Peach", "Apple", "Banana");
+            string maxValue = compare.MaxStringValue("Apple", "Banana", "Peach");
             Console.WriteLine(maxValue);
         }
     }
