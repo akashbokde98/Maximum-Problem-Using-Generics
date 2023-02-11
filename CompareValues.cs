707969f158dee4ca7034d26abb700ef2f88465e3
akashbokde98 <akashbokde98@gmail.com>
2023-02-12 00:36:45 +0530

    Added: Maximum int,float and String Value

---
+++ CompareValues.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestMaximum
+{
+    internal class CompareValues
+    {
+        public T MaximumValue<T>(T firstValue, T secondValue, T thirdValue)where T : IComparable
+        {
+            if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0)
+            {
+                return firstValue;
+            }
+            if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0)
+            {
+                return secondValue;
+            }
+            if (thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) > 0)
+            {
+                return thirdValue;
+            }
+            return firstValue;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,7 +6,14 @@ namespace TestMaximum
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Find Maximum Using Generics");
+            CompareValues compare = new CompareValues();
+
+            int maxIntegerValue = compare.MaximumValue<int>(30, 20, 10);
+            float maxFloatValue = compare.MaximumValue<float>(30.33f, 20.22f, 10.55f);
+            string maxStringValue = compare.MaximumValue<string>("Apple", "Peach", "Banana");
+            Console.WriteLine(maxIntegerValue);
+            Console.WriteLine(maxFloatValue);
+            Console.WriteLine(maxStringValue);
         }
     }
 }
